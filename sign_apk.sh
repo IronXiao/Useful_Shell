7a99990ce2943cac258f80b3c8f8348ba5d18d46
xiaoleigs <xiaoleigs@gmail.com>
2017-12-19 10:33:26 +0800

    Add output message to make this shell script friendly.

--- sign_apk.sh
+++ sign_apk.sh
@@ -3,8 +3,13 @@ if [ 1 -gt $# ]; then
     echo "Usage ./sign.sh apk_name"
     exit 1
 fi
+set -e
 APK=$1
 PEM=platform.x509.pem
 PK8=platform.pk8
 SIGNAPK=signapk.jar
 java -Xmx2048m -jar $SIGNAPK -w $PEM $PK8 $APK ${APK%.*}_signed.${APK##*.}
+if [ $? -eq 0 ];then
+echo  "\n\tsign apk "$APK" success !\n"
+fi
+
